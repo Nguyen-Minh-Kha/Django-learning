08706e55b337b7237ec109ceb7c93b49dcc5602a
Nguyen-Minh-Kha <pro.nguyenminhkha@gmail.com>
2023-09-20 11:09:26 +0200

    raise exception 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Question
 
+from django.http import Http404
+
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -17,7 +19,11 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail (request, question_id):
-    return HttpResponse("you're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, "polls/detail.html",{"question": question})
 
 def results(request, question_id):
     response = "you're looking at the results of question %s."
